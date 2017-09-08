32fce1b480de3c62325bbb4b7e782b944e31598c
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-09-08 12:00:11 +0200

    Remove debug

--- src/Pckg/Impero/Api/Endpoint/User.php
+++ src/Pckg/Impero/Api/Endpoint/User.php
@@ -8,13 +8,9 @@ class User extends Endpoint
 
     public function create($data = [])
     {
-        d('posting', $data);
         $this->api->postApi('user', $data);
 
-        d('api response', $this->api->getApiResponse());
         $this->data = new Object($this->api->getApiResponse('user'));
-
-        dd('data', $this->data);
 
         return $this;
     }
